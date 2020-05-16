9cd6f7f5465945133cd2cec83cff0a6e921fa2a7
andrew <andrew_mokryj@ukr.net>
2020-05-16 20:59:47 +0300

    Add diagram widget

---
+++ lib/views/diagram_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutterappweb/helpers/utils.dart';
+import 'package:flutterappweb/model/panel_model.dart';
+
+class DiagramWidget extends StatelessWidget{
+  final Panel panel;
+
+  const DiagramWidget({Key key, this.panel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double w = getWidth(context);
+    double h = getHeight(context);
+
+    return FutureBuilder(
+      future: panel != null ? getPanelTodayLogs(panel) : getAlllTodayLogs(context),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final logMaps = snapshot.data as List;
+//              return Container(
+//                height: 40,
+//                color: Colors.blue,
+//              );
+          return Column(
+            children: <Widget>[
+
+            ]
+          );
+        }
+        else {
+          return Container();
+        }
+      },
+    ) ;
+  }
+}
