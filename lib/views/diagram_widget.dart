ae549c60420221193e8de4c10f11e0ac1d369696
andrew <andrew_mokryj@ukr.net>
2020-05-17 19:50:51 +0300

    Use syncfusion_flutter_charts

--- lib/views/diagram_widget.dart
+++ lib/views/diagram_widget.dart
@@ -1,10 +1,10 @@
-import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:SUNMAX/helpers/utils.dart';
 import 'package:SUNMAX/model/panel_model.dart';
 import 'package:SUNMAX/model/log_model.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class DiagramWidget extends StatelessWidget{
+class DiagramWidget extends StatelessWidget {
   final Panel panel;
 
   const DiagramWidget({Key key, this.panel}) : super(key: key);
@@ -15,7 +15,8 @@ class DiagramWidget extends StatelessWidget{
     double h = getHeight(context);
 
     return FutureBuilder(
-      future: panel != null ? getPanelTodayLogs(panel) : getAlllTodayLogs(context),
+      future: panel != null ? getPanelTodayLogs(panel) : getAlllTodayLogs(
+        context),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final times = [
@@ -45,11 +46,11 @@ class DiagramWidget extends StatelessWidget{
             '23',
           ];
           var logMaps = snapshot.data as List<Log>;
-//          logMaps.removeWhere((element) => element.produced == 0);
 
           times.forEach((time) {
-            var logs = logMaps.where((element) => element.time.startsWith(time));
-            if (logs.length == 0){
+            var logs = logMaps.where((element) =>
+              element.time.startsWith(time));
+            if (logs.length == 0) {
               Log newLog = Log(
                 time: "${time}:00",
                 produced: 0,
@@ -61,37 +62,47 @@ class DiagramWidget extends StatelessWidget{
           logMaps.sort((l1, l2) => l1.time.compareTo(l2.time));
 
           var series = [
-            charts.Series(
-              id: 'Produced',
-              domainFn: (Log logData, _) => logData.time.substring(0, 2),
-              measureFn: (Log logData, _) => logData.produced / 3600000,
-              colorFn: (Log logData, _) => charts.Color(r: 255, g: 255, b: 0),
-              data: logMaps,
-            ),
+            ColumnSeries<Log, String>(
+              dataSource: logMaps,
+              xValueMapper: (Log log, _) => log.time.substring(0, 2),
+              yValueMapper: (Log log, _) => log.produced / 3600000,
+              legendItemText: "Вироблено",
+            )
           ];
 
-          if(panel == null){
-            series.add(charts.Series(
-              id: 'Given',
-              domainFn: (Log logData, _) => logData.time.substring(0, 2),
-              measureFn: (Log logData, _) => logData.given / 3600000,
-              colorFn: (Log logData, _) => charts.Color(r: 0, g: 255, b: 0),
-              data: logMaps,
+          if (panel == null) {
+            series.add(ColumnSeries<Log, String>(
+              dataSource: logMaps,
+              xValueMapper: (Log log, _) => log.time.substring(0, 2),
+              yValueMapper: (Log log, _) => log.given / 3600000,
+              legendItemText: "Продано",
             ));
           }
 
           return Container(
             height: h / 2,
-            child: charts.BarChart(
-              series,
-              animate: false,
-            ),
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(
+                title: AxisTitle(
+                  text: 'Час',
+                )
+              ),
+              primaryYAxis: NumericAxis(
+                title: AxisTitle(
+                  text: 'кВат * год',
+                )
+              ),
+              title: ChartTitle(text: 'Виробіток сьогодні:'),
+              legend: Legend(isVisible: true),
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: series,
+            )
           );
         }
         else {
           return Container();
         }
       },
-    ) ;
+    );
   }
 }
