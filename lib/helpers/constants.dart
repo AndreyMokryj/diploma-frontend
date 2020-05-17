ddddbde072ea49083c753ad1868272bda0c0eeed
andrew <andrew_mokryj@ukr.net>
2020-05-17 21:29:35 +0300

    Some refactor

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 //const baseUrl = kReleaseMode ? "https://mavlabs.hopto.org/" : "http://localhost:4444/" ;
 const baseUrl = "http://localhost:4444/app/" ;
 
@@ -21,6 +23,45 @@ const monthNames = {
   '12' : 'Грудня',
 };
 
+const times = [
+  '00',
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+];
+
 const smallLimit = 600;
 const mediumLimit = 900;
 const largeLimit = 1200;
+
+const legendItems = [
+  {
+    'title':'Вироблено',
+    'color': Colors.green,
+  },
+  {
+    'title':'Продано',
+    'color': Colors.orange,
+  },
+
+];
--- lib/views/diagram_widget.dart
+++ lib/views/diagram_widget.dart
@@ -22,32 +22,6 @@ class DiagramWidget extends StatelessWidget {
         context),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          final times = [
-            '00',
-            '01',
-            '02',
-            '03',
-            '04',
-            '05',
-            '06',
-            '07',
-            '08',
-            '09',
-            '10',
-            '11',
-            '12',
-            '13',
-            '14',
-            '15',
-            '16',
-            '17',
-            '18',
-            '19',
-            '20',
-            '21',
-            '22',
-            '23',
-          ];
           var logMaps = snapshot.data as List<Log>;
 
           times.forEach((time) {
@@ -64,25 +38,22 @@ class DiagramWidget extends StatelessWidget {
           });
           logMaps.sort((l1, l2) => l1.time.compareTo(l2.time));
 
-          var series = [
+          var list = [0];
+          if (panel == null) {
+            list.add(1);
+          }
+
+          var series = list.map((e) =>
             ColumnSeries<Log, String>(
               dataSource: logMaps,
               xValueMapper: (Log log, _) => log.time.substring(0, 2),
-              yValueMapper: (Log log, _) => log.produced / 3600000,
-              legendItemText: "Вироблено",
-              color: Colors.green,
-            )
-          ];
-
-          if (panel == null) {
-            series.add(ColumnSeries<Log, String>(
-              dataSource: logMaps,
-              xValueMapper: (Log log, _) => log.time.substring(0, 2),
-              yValueMapper: (Log log, _) => log.given / 3600000,
-              legendItemText: "Продано",
-              color: Colors.orange,
-            ));
-          }
+              yValueMapper: (Log log, _) =>
+              e == 0
+                ? log.produced / 3600000
+                : log.given / 3600000,
+              legendItemText: legendItems[e]['title'],
+              color: legendItems[e]['color'],
+            )).toList();
 
           return Column(
             children: <Widget>[
@@ -90,19 +61,15 @@ class DiagramWidget extends StatelessWidget {
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Container(
-                  width: max(w, mediumLimit * 1.0),
+                  width: w < mediumLimit ? mediumLimit * 1.0 :
+                  (w < largeLimit ? w * 0.95 : w * 0.75),
                   child: SfCartesianChart(
-                    primaryXAxis: CategoryAxis(
-//                      title: AxisTitle(
-//                        text: 'Час',
-//                      )
-                    ),
+                    primaryXAxis: CategoryAxis(),
                     primaryYAxis: NumericAxis(
                       title: AxisTitle(
                         text: 'кВат * год',
                       )
                     ),
-//                title: ChartTitle(text: 'Виробіток сьогодні:'),
                     legend: Legend(isVisible: w >= mediumLimit),
                     tooltipBehavior: TooltipBehavior(enable: true),
                     series: series,
@@ -114,43 +81,22 @@ class DiagramWidget extends StatelessWidget {
                 height: 10,
               ),
               w < mediumLimit ? Column(
-                children: <Widget>[
+                children: legendItems.map((el) =>
                   Row(
                     children: <Widget>[
-                      SizedBox(
-                        width: 20,
-                      ),
                       Container(
+                        margin: EdgeInsets.only(
+                          top: 10,
+                          left: 20,
+                          right: 20,
+                        ),
                         width: 20,
                         height: 20,
-                        color: Colors.green,
+                        color: el['color'],
                       ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text("Вироблено")
+                      Text(el['title'])
                     ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Container(
-                        width: 20,
-                        height: 20,
-                        color: Colors.orange,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text("Продано")
-                    ],
-                  )
-                ],
+                  )).toList()
               ) : Container(),
             ],
           );
